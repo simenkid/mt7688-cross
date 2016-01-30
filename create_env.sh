301a96149f238a4c38cf417de958f32d38d693e2
Simen Li <simenkid@gmail.com>
2016-01-30 12:41:35 +0800

    uncomment make statements

--- create_env.sh
+++ create_env.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "****************************************************************"
-echo "* MT7688 Linkit Smart cross-environment for build node modules *"
-echo "* Tested on:                                                   *"
-echo "*     Host: Ubuntu 14.04LTS x64                                *"
-echo "*     Target: MT7688 MIPS24KEc little endian, OpenWRT          *"
-echo "*                                                              *"
-echo "* Author: Simen Li (simenkid@gmail.com)                        *"
-echo "* GitHub: https://github.com/simenkid/mt7688-cross             *"
-echo "* License: MIT                                                 *"
-echo "****************************************************************"
+echo "*******************************************************************"
+echo "* MT7688 Linkit Smart cross-environment for building node modules *"
+echo "* Tested on:                                                      *"
+echo "*     Host: Ubuntu 14.04LTS x64                                   *"
+echo "*     Target: MT7688 MIPS24KEc little endian, OpenWRT             *"
+echo "*                                                                 *"
+echo "* Author: Simen Li (simenkid@gmail.com)                           *"
+echo "* GitHub: https://github.com/simenkid/mt7688-cross                *"
+echo "* License: MIT                                                    *"
+echo "*******************************************************************"
 
 echo "Please make sure you have installed the following packages on you host: "
 echo "  subversion, build-essential, gcc-multilib, g++-multilib"
@@ -44,6 +44,7 @@ else
     # fi
 fi
 
+echo " "
 echo "Create target folder..."
 if [ ! -d $(pwd)/linkit ]; then
     mkdir $(pwd)/linkit
@@ -52,26 +53,29 @@ if [ ! -d $(pwd)/linkit ]; then
     fi
 fi
 
+echo " "
 echo "Get MT7688 OpenWrt SDK and decompress it into /mt7688sdk..."
 if [ ! -f OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2 ]; then
     wget http://download.labs.mediatek.com/MediaTek_LinkIt_Smart_7688_Openwrt_sdk_Linux -O OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
 fi
 
 if [ ! -d mt7688sdk ]; then
-    tar xjvf OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
+    tar xjvf OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2 > /dev/null
     mv OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/ mt7688sdk
 fi
 
+echo " "
 echo "Get node source and decompress it into /node-v0.12.7-mips..."
 if [ ! -f node-v0.12.7.tar.gz ]; then
     wget https://nodejs.org/dist/v0.12.7/node-v0.12.7.tar.gz
 fi
 
 if [ ! -d node-v0.12.7-mips ]; then
-    tar xzvf node-v0.12.7.tar.gz
+    tar xzvf node-v0.12.7.tar.gz > /dev/null
     mv node-v0.12.7/ node-v0.12.7-mips
 fi
 
+echo " "
 echo "Get depot tools for google chromium repository..."
 if [ ! -d depot_tools ]; then
     if [ -f .gclient ]; then
@@ -86,6 +90,7 @@ fi
 # source env.sh to export environment varaible for cross-toolchain
 . $(pwd)/env.sh
 
+echo " "
 echo "Get v8 source into /v8"
 echo "Takes few minutes, please wait..."
 echo " "
@@ -107,33 +112,36 @@ if [ ! -d v8 ]; then
     cd ..
 fi
 
+echo " "
 echo "Build v8 for mips platform..."
 echo "Takes around 10 minutes, please wait..."
 echo " "
 
 cd v8/
-# make clean
-# make distclean
-# make dependencies
+make clean
+make distclean
+make dependencies
 
-# make mipsel.release werror=no library=shared snapshot=off -j4
-# cp ${BASEDIR}/v8/out/mipsel.release/lib.target/libicui18n.so ${LIBPATH}
-# cp ${BASEDIR}/v8/out/mipsel.release/lib.target/libicuuc.so ${LIBPATH}
+make mipsel.release werror=no library=shared snapshot=off -j4
+cp ${BASEDIR}/v8/out/mipsel.release/lib.target/libicui18n.so ${LIBPATH}
+cp ${BASEDIR}/v8/out/mipsel.release/lib.target/libicuuc.so ${LIBPATH}
 cd ..
 
 # Build Node.js
+echo " "
 echo "Build node.js for mips platform..."
 echo "Takes few minutes, please wait..."
 echo " "
 
 cd node-v0.12.7-mips/
-# make clean
-# make distclean
+make clean
+make distclean
 
-# ./configure --prefix=${TARGET_PATH} --dest-cpu=mipsel --dest-os=linux --without-snapshot --shared-v8 --shared-v8-includes=${V8SOURCE}/include/ --shared-v8-libpath=${V8SOURCE}/out/mipsel.release/lib.target/
-# make snapshot=off -j4
-# make install
+./configure --prefix=${TARGET_PATH} --dest-cpu=mipsel --dest-os=linux --without-snapshot --shared-v8 --shared-v8-includes=${V8SOURCE}/include/ --shared-v8-libpath=${V8SOURCE}/out/mipsel.release/lib.target/
+make snapshot=off -j4
+make install
 
+echo " "
 echo "*********************************************"
 echo "* Cross-environment done!                   *"
 echo "*                                           *"
